77900302efeb0b6f7c0e9d2ecd29d4540b4a255f
fcoJCC <fcoJCC@users.noreply.github.com>
2015-09-08 00:31:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+#ifndef GRAFO_H
+#include "grafo.h"
+#define GRAFO_H
+#endif
+
+
 
