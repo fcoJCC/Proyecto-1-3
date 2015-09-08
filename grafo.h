00828d24e7827b529f3f24c2a9c20e3cb18c81b9
fcoJCC <fcoJCC@users.noreply.github.com>
2015-09-08 00:41:06 -0400

    Update grafo.h

--- grafo.h
+++ grafo.h
@@ -1,1 +1,19 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+
+typedef struct type_graph{
+  int cant_conocidos;//basado en una red de conocidos, puede ser la etiqueta que tenga.
+  int pasos;//cantidad de pasos que ha tomado hasta llegar al nodo actual.
+  char marca;//blanca"B"   gris"G"  negra"N", indicara el color actual.
+  
+}grafo;
+
 
