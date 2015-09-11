95a09df9a3f5395fb60c610a48f9c395a820d631
fcoJCC <fcoJCC@users.noreply.github.com>
2015-09-10 22:03:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -13,5 +13,28 @@
 #define GRAFO_H
 #endif
 
+typedef struct tipo_cola{
+    struct tipo_cola *sig, *pri, *ult;
+}Cola;
+//funcion que crea e inicializa una cola
+Cola *inicializar_cola(){
+    Cola *aux_cola=(Cola *)malloc(sizeof(struct tipo_cola));
+
+    aux_cola->primero=NULL;
+    aux_cola->sgte=NULL;
+    aux_cola->ultimo=NULL;
+
+    return aux_cola;
+}
+//funcion que verifica si la cola esta vacia
+int vacia_cola(Cola *cola){
+  
+  if(aux_cola->primero==NULL){
+    return 1;//esta vacia 
+  }
+   return 0; 
+}
+
+
 
 
